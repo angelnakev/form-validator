5701596face71e4b9a118575041726dccfe42e00
angelnakev <angelnakev@outlook.com>
2023-02-11 22:44:36 +0100

    functions isValidEmail and checkRequired added, event updated

--- main.js
+++ main.js
@@ -3,26 +3,38 @@ const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const password2 = document.querySelector("#password2");
-const small = document.querySelector("small");
 
 function showError(input, msg) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
 
+  const small = formControl.querySelector('small')
   small.textContent = msg;
 }
 
 function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${input.id} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
+  checkRequired([username, email, password, password2]);
 });
